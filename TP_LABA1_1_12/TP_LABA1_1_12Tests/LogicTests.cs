c048f538731c51059ebbbc7d419c2e4fcd046310
Ilyouha852 <ilaivanov2899@gmail.com>
2025-02-23 13:04:27 +0800

    Created tests for program

---
+++ TP_LABA1_1_12/TP_LABA1_1_12Tests/LogicTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void ConvertorTest()
+        {
+            //В этом тесте метод Convertor должен вывести "1 рубль"
+            int cost = 100;
+            var message = Logic.Convertor(cost);
+            Assert.AreEqual("1 рубль", message);
+        }
+
+        [TestMethod()]
+        public void ConvertorTest1()
+        {
+            //В этом тесте метод Convertor должен вывести "1 копейка"
+            int cost = 1;
+            var message = Logic.Convertor(cost);
+            Assert.AreEqual("1 копейка", message);
+        }
+
+        [TestMethod()]
+        public void ConvertorTest2()
+        {
+            //В этом тесте метод Convertor должен вывести "1 рубль, 1 копейка"
+            int cost = 101;
+            var message = Logic.Convertor(cost);
+            Assert.AreEqual("1 рубль, 1 копейка", message);
+        }
+
+        [TestMethod()]
+        public void ConvertorTest3()
+        {
+            //В этом тесте метод Convertor должен вывести "2 рубля"
+            int cost = 200;
+            var message = Logic.Convertor(cost);
+            Assert.AreEqual("2 рубля", message);
+        }
+
+        [TestMethod()]
+        public void ConvertorTest4()
+        {
+            //В этом тесте метод Convertor должен вывести "2 копейки"
+            int cost = 2;
+            var message = Logic.Convertor(cost);
+            Assert.AreEqual("2 копейки", message);
+        }
+
+        [TestMethod()]
+        public void ConvertorTest5()
+        {
+            //В этом тесте метод Convertor должен вывести "3 рубля, 4 копейки"
+            int cost = 304;
+            var message = Logic.Convertor(cost);
+            Assert.AreEqual("3 рубля, 4 копейки", message);
+        }
+
+        [TestMethod()]
+        public void ConvertorTest6()
+        {
+            //В этом тесте метод Convertor должен вывести "11 рублей, 5 копеек"
+            int cost = 1105;
+            var message = Logic.Convertor(cost);
+            Assert.AreEqual("11 рублей, 5 копеек", message);
+        }
+    }
+}
