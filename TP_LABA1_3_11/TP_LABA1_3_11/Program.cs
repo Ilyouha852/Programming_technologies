66d509e13d48502f09708c70b1fa4cf331b8748e
Ilyouha852 <ilaivanov2899@gmail.com>
2025-02-24 21:53:38 +0800

    Uploaded 3rd laboratory work with tests

---
+++ TP_LABA1_3_11/TP_LABA1_3_11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string word1 = "";
+        while (word1 == "")
+        {
+            Console.Write("Введите первое слово: ");
+            word1 = Console.ReadLine();
+        }
+
+        string word2 = "";
+        while (word2 == "")
+        {
+            Console.Write("Введите второе слово: ");
+            word2 = Console.ReadLine();
+        }
+
+        string msg = Logic.haveSameLetters(word1, word2);
+        Console.Write(msg);
+    }
+}
+
+public class Logic
+{
+    public static string haveSameLetters(string word1, string word2)
+    {
+        string msg = "";
+        HashSet<char> uniqueChars = new HashSet<char>(word1);
+
+        foreach (char ch in uniqueChars)
+        {
+            if (word2.Contains(ch)) msg += "да ";
+            else msg += "нет ";
+        }
+
+        return msg;
+    }    
+} 
---
+++ TP_LABA1_3_11/TP_LABA1_3_11Tests/LogicTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void haveSameLettersTest()
+        {
+            string word1 = "процессор";
+            string word2 = "информация";
+
+            string msg = Logic.haveSameLetters(word1, word2);
+
+            Assert.AreEqual("нет да да да нет нет ", msg);
+        }
+
+        [TestMethod()]
+        public void haveSameLettersTest1()
+        {
+            string word1 = "Привет";
+            string word2 = "пока";
+
+            string msg = Logic.haveSameLetters(word1, word2);
+
+            Assert.AreEqual("нет нет нет нет нет нет ", msg);
+        }
+    }
+}
