7124c84ccb9ee000ebdba730b58109ac7a5d82fc
Ilyouha852 <ilaivanov2899@gmail.com>
2025-05-12 22:55:14 +0800

    Reloaded TP_LABA6_8

---
+++ TP_LABA6_8/Programming_technologies_6/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Programming_technologies_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbDirection = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDirection = new System.Windows.Forms.Label();
+            this.tbSpreading = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSpreading = new System.Windows.Forms.Label();
+            this.tbSpeed = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkDebug = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbDirection)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpreading)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDisplay
+            // 
+            this.picDisplay.Location = new System.Drawing.Point(12, 12);
+            this.picDisplay.Name = "picDisplay";
+            this.picDisplay.Size = new System.Drawing.Size(776, 426);
+            this.picDisplay.TabIndex = 0;
+            this.picDisplay.TabStop = false;
+            this.picDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDisplay_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbDirection
+            // 
+            this.tbDirection.Location = new System.Drawing.Point(12, 474);
+            this.tbDirection.Maximum = 359;
+            this.tbDirection.Name = "tbDirection";
+            this.tbDirection.Size = new System.Drawing.Size(380, 45);
+            this.tbDirection.TabIndex = 1;
+            this.tbDirection.Scroll += new System.EventHandler(this.tbDirection_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 458);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Направление";
+            // 
+            // lblDirection
+            // 
+            this.lblDirection.AutoSize = true;
+            this.lblDirection.Location = new System.Drawing.Point(399, 474);
+            this.lblDirection.Name = "lblDirection";
+            this.lblDirection.Size = new System.Drawing.Size(0, 13);
+            this.lblDirection.TabIndex = 3;
+            // 
+            // tbSpreading
+            // 
+            this.tbSpreading.Location = new System.Drawing.Point(421, 474);
+            this.tbSpreading.Maximum = 359;
+            this.tbSpreading.Name = "tbSpreading";
+            this.tbSpreading.Size = new System.Drawing.Size(329, 45);
+            this.tbSpreading.TabIndex = 4;
+            this.tbSpreading.Scroll += new System.EventHandler(this.tbSpreading_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 458);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Разброс";
+            // 
+            // lblSpreading
+            // 
+            this.lblSpreading.AutoSize = true;
+            this.lblSpreading.Location = new System.Drawing.Point(753, 486);
+            this.lblSpreading.Name = "lblSpreading";
+            this.lblSpreading.Size = new System.Drawing.Size(0, 13);
+            this.lblSpreading.TabIndex = 6;
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.Location = new System.Drawing.Point(756, 474);
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.Size = new System.Drawing.Size(266, 45);
+            this.tbSpeed.TabIndex = 7;
+            this.tbSpeed.Scroll += new System.EventHandler(this.tbSpeed_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(753, 458);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Скорость частиц";
+            // 
+            // chkDebug
+            // 
+            this.chkDebug.AutoSize = true;
+            this.chkDebug.Location = new System.Drawing.Point(812, 35);
+            this.chkDebug.Name = "chkDebug";
+            this.chkDebug.Size = new System.Drawing.Size(105, 17);
+            this.chkDebug.TabIndex = 9;
+            this.chkDebug.Text = "Режим отладки";
+            this.chkDebug.UseVisualStyleBackColor = true;
+            this.chkDebug.CheckedChanged += new System.EventHandler(this.chkDebug_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 517);
+            this.Controls.Add(this.chkDebug);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbSpeed);
+            this.Controls.Add(this.lblSpreading);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbSpreading);
+            this.Controls.Add(this.lblDirection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDirection);
+            this.Controls.Add(this.picDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbDirection)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpreading)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar tbDirection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDirection;
+        private System.Windows.Forms.TrackBar tbSpreading;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSpreading;
+        private System.Windows.Forms.TrackBar tbSpeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkDebug;
+    }
+}
+
---
+++ TP_LABA6_8/Programming_technologies_6/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Programming_technologies_6.Emitter;
+using static Programming_technologies_6.Particle;
+
+namespace Programming_technologies_6
+{
+    public partial class Form1 : Form
+    {
+        List<Emitter> emitters = new List<Emitter>();
+        Emitter emitter;
+
+        GravityPoint point1; // добавил поле под первую точку
+        GravityPoint point2; // добавил поле под вторую точку
+        Emitter.RadarPoint radar;
+
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            this.emitter = new Emitter // создаю эмиттер и привязываю его к полю emitter
+            {
+                Direction = 0,
+                Spreading = 10,
+                SpeedMin = 10,
+                SpeedMax = 10,
+                ColorFrom = Color.Gold,
+                ColorTo = Color.Red,
+                ParticlesPerTick = 10,
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2,
+            };
+
+            emitters.Add(this.emitter);
+
+
+            // привязываем гравитоны к полям
+            point1 = new GravityPoint
+            {
+                X = picDisplay.Width / 2 + 100,
+                Y = picDisplay.Height / 2,
+            };
+            point2 = new GravityPoint
+            {
+                X = picDisplay.Width / 2 - 100,
+                Y = picDisplay.Height / 2,
+            };
+            radar = new Emitter.RadarPoint()
+            {
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2
+            };
+            emitter.impactPoints.Add(radar);
+
+
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            radar.ResetCounter();
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g); // а тут теперь рендерим через эмиттер
+            }
+
+            picDisplay.Invalidate();
+        }
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            foreach (var emitter in emitters)
+            {
+                emitter.MousePositionX = e.X;
+                emitter.MousePositionY = e.Y;
+            }
+
+            // а тут передаем положение мыши, в положение гравитона
+            point2.X = e.X;
+            point2.Y = e.Y;
+            radar.X = e.X;
+            radar.Y = e.Y;
+
+        }
+
+        private void tbDirection_Scroll(object sender, EventArgs e)
+        {
+            emitter.Direction = tbDirection.Value;
+            lblDirection.Text = $"{tbDirection.Value}°";
+        }
+
+        private void tbSpreading_Scroll(object sender, EventArgs e)
+        {
+            emitter.Spreading = tbSpreading.Value;
+            lblSpreading.Text = $"{tbSpreading.Value}°";
+        }
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            radar.ChangeRadius(e.Delta > 0 ? 10 : -10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbSpeed_Scroll(object sender, EventArgs e)
+        {
+            emitter.SpeedMultiplier = tbSpeed.Value / 10f;
+        }
+
+        private void chkDebug_CheckedChanged(object sender, EventArgs e)
+        {
+            emitter.DebugMode = chkDebug.Checked;
+        }
+    }
+}
