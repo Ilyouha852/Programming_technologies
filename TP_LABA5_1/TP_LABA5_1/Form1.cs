b4acf36c8e18f037e3e823101dd263872b5b3bd6
Ilyouha852 <ilaivanov2899@gmail.com>
2025-05-12 22:45:14 +0800

    Uploaded TP_LABA5_1

---
+++ TP_LABA5_1/TP_LABA5_1/Form1.cs
@@ -0,0 +1,121 @@
+using TP_LABA5_1.Objects;
+
+namespace TP_LABA5_1
+{
+    public partial class Form1 : Form
+    {
+        List<BaseObject> objects = new();
+        Player player;
+        Marker marker;
+        Random random = new Random();
+        List<Circle> circles = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+
+            player.OnOverLap += (p, obj) =>
+            {
+                txtLog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] Игрок пересекся с {obj}\n" + txtLog.Text;
+
+                if (obj is Circle circle)
+                {
+                    circles.Remove(circle);
+                    objects.Remove(circle);
+
+                    var newCircle = new Circle(random.Next(50, pbMain.Width - 50), random.Next(50, pbMain.Height - 50), 20);
+
+                    circles.Add(newCircle);
+                    objects.Add(newCircle);
+                }
+            };
+
+            player.OnMarkerOverlap += (m) =>
+            {
+                objects.Remove(m);
+                marker = null;
+            };
+
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
+
+            objects.Add(marker);
+
+            for (int i = 0; i < 5; i++)
+            {
+                var newCircle = new Circle(random.Next(50, pbMain.Width - 50), random.Next(50, pbMain.Height - 50), 20);
+                circles.Add(newCircle);
+                objects.Add(newCircle);
+            }
+
+            objects.Add(player); 
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(Color.White);
+
+            updatePlayer();
+
+            foreach (var obj in objects.ToList())
+            {
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    player.Overlap(obj);
+                    obj.Overlap(player);
+                }
+
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+        }
+
+        private void updatePlayer()
+        {
+            if (marker != null)
+            {
+                float dx = marker.X - player.X;
+                float dy = marker.Y - player.Y;
+
+                float length = MathF.Sqrt(dx * dx + dy * dy);
+                dx /= length;
+                dy /= length;
+
+                player.vX += dx * 0.5f;
+                player.vY += dy * 0.5f;
+
+                player.Angle = 90 - MathF.Atan2(player.vX, player.vY) * 180 / MathF.PI;
+            }
+
+            player.vX += -player.vX * 0.1f;
+            player.vY += -player.vY * 0.1f;
+
+            player.X += player.vX;
+            player.Y += player.vY;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pbMain.Invalidate();
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (marker == null)
+            {
+                marker = new Marker(0, 0, 0);
+                objects.Add(marker);
+            }
+
+            marker.X = e.X;
+            marker.Y = e.Y;
+        }
+    }
+}
---
+++ TP_LABA5_1/TP_LABA5_1/Objects/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace TP_LABA5_1.Objects
+{
+    internal class Circle : BaseObject
+    {
+        private float radius;
+
+        public Circle(float x, float y, float radius) : base(x, y, 0)
+        {
+            this.radius = radius;
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = new GraphicsPath();
+            path.AddEllipse(-radius, -radius, 2 * radius, 2 * radius);
+            return path;
+        }
+
+        public override void Render(Graphics g)
+        {
+            using (var brush = new SolidBrush(Color.Red))
+            {
+                g.FillEllipse(brush, -radius, -radius, 2 * radius, 2 * radius);
+            }
+        }
+
+        public float Radius => radius;
+    }
+}
+
